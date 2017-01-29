765fb74591b125c6264e5f9e684258baed59e025
Shaunprado <Shaunprado@hotmail.com>
2017-01-29 01:25:54 +0000

    Added lobby feature and improved battling system

--- browser.py
+++ browser.py
@@ -5,12 +5,15 @@ import time
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 global wait
+global driver
 
 def main():
-	Username = "AuuxL"
-	Password = "appleman"
-	login(Username, Password)
-	homepage();
+	Username = "X"
+	Password = "X"
+	driver = login(Username, Password);
+	homepage(driver);
+	selenium.stop
+
 
 def findButton(name,driver):
 
@@ -20,21 +23,90 @@ def findButton(name,driver):
 		if button.text.strip() == name:
 			button.click()
 
+def battle(driver):
+	print("Which starter pokemon would you like to choose")
+	starter = int(raw_input("0-5 to pick"))
 
-def findRandomBattle(username, driver):
-	time.sleep(2)
-	driver.find_element_by_name('search').click()
-	print("Battle started")
-	time.sleep(2)
-	driver.find_element_by_name('chooseMove').click()
 
+def findRandomBattle(driver):
+	while(true):
+		battle = int(raw_input("Would you like to battle? 1 for another battle, 0 to exit"))
+		if battle == 1:
+			driver.find_element_by_name('search').click()
+			time.sleep(2)
+			print("Battle started")
+			time.sleep(2)
+			battle(driver);
+		if battle == 0:
+			break
 
 def gotoLobby(driver):
 	time.sleep(2)
-	driver.find_element_by_name('').click()
+	driver.find_element_by_xpath("//a[@href='/lobby']").click();
+	lobby();
 
-def loadTeam():
+def loadTeam(driver):
+	'''print("Enter the name of the file your team is stored in")
+	team = raw_input("Enter input: ")'''
+	time.sleep(2)
+	teamConfig = open("team.txt", "r")
 
+	driver.find_element_by_name('joinRoom').click()
+	time.sleep(2)
+	driver.find_element_by_name('newTop').click()
+	time.sleep(2)
+	driver.find_element_by_name('import').click()
+	time.sleep(2)
+	
+	textboxes = driver.find_elements_by_class_name('textbox')
+	for textbox in textboxes:
+		if textbox.text == None:
+			textbox.send_keys(teamConfig.read())
+			break
+
+	teamConfig.close()
+
+	driver.find_element_by_name('saveImport').click()
+
+	time.sleep(5)
+
+	driver.find_element_by_css_selector('.select.formatselect.teambuilderformatselect').click()
+	time.sleep(2)
+	driver.find_element_by_name('selectFormat').click()
+	sleep(2)
+	driver.find_element_by_css_selector('.button.disabled').click()
+	time.sleep(2)
+
+	buttons = driver.find_elements_by_class_name('home')
+
+	for home in buttons:
+		if home.text.strip() == "Home":
+			home.click()
+			break
+
+
+def lobby():
+	return 0
+
+def homepage(driver):
+	print("Would you like to import teams? Keep the text file in the same directory as this file. (1 for yes, 0 for no")
+	shouldLoadTeam = int(raw_input("Enter your answer: "))
+	if shouldLoadTeam == 1:
+		loadTeam(driver);
+
+	time.sleep(2)
+	text = 0
+	while text != 1 or text != 2:
+		print("Do you want to random battle (1) or go to the lobby (2) ?")
+		text = int(raw_input("Enter your answer: "))
+		time.sleep(1)
+
+		if text == 1:
+			findRandomBattle(driver);
+			break;
+		elif text == 2:
+			gotoLobby(driver)
+			break;
 
 def login(Username, Password):
 	driver = webdriver.Chrome('/home/auuxl/Downloads/chromedriver')
@@ -55,22 +127,7 @@ def login(Username, Password):
 
 	time.sleep(2)
 	findButton("Choose name", driver);
-
-	time.sleep(2)
-	text = 0
-	while text != 1 or text != 2:
-		print("Do you want to random battle (1) or go to the lobby (2) ?")
-		text = int(raw_input("Enter your answer: "))
-		sleep(1)
-
-		if text == 1:
-			findRandomBattle("Shaun", driver);
-		elif text == 2:
-			gotoLobby(driver)
-
-
-	selenium.stop
-
+	return driver
 
 
 if __name__ == '__main__':
